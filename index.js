a449bfd8fc19f48c4d51a742ed098fcebe99e5d3
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-02-12 12:37:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function addComment() {
   event.preventDefault();
   var bodyText = document.getElementById("commentText").value;
   var commenter = document.getElementById("commenterName").value;
- 
+
   var commentBody = createCommentBody(bodyText);
   var commenterLabel = createCommenterLabel(commenter);
   postNewComment(commentBody, commenterLabel);
